c212a0359f6e571e0695caae1f5a9ec66f7b08e9
PickleOgre <skelkor2@gmail.com>
2025-05-19 21:03:19 -0700

    add styles to header, footer, and layout

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -2,7 +2,7 @@ import PreviewLink from "./preview-link"
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
+    <footer className="bg-[#eee8d5] border-t border-[#93a1a1]">
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
@@ -11,7 +11,7 @@ const Footer = () => {
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="#"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-[#002b36] hover:bg-[#fdf6e3] hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Read Documentation
             </a>
--- components/misc/layout.tsx
+++ components/misc/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+    <div className="flex flex-col min-h-screen overflow-hidden bg-[#fdf6e3]">
       <Header />
       <main className="grow">
         {children}
